7d00dd4bef162d2e370d6e8f3be290980ae1ffa6
abhigowda27 <136908697+abhigowda27@users.noreply.github.com>
2025-01-18 16:49:02 +0530

    Delete lib directory

--- lib/controller/permission.dart
+++
@@ -1,9 +0,0 @@
-import 'package:permission_handler/permission_handler.dart';
-
-Future<void> requestPermission(Permission permission) async {
-  final status = await permission.request();
-  // print(status);
-  if (status.isDenied) {
-    requestPermission(permission);
-  }
-}
